d21f5a48b30c4bc309585844036c83c5d6208407
RotemAAA <rotem.guzman@gmail.com>
2018-03-12 17:21:39 +0200

    Drop off center Inventory fL DeliveryGuy

--- app/src/main/java/com/guzman/rotem/tamalsocialbank1/StorageMainActivity.java
+++ app/src/main/java/com/guzman/rotem/tamalsocialbank1/StorageMainActivity.java
@@ -7,17 +7,18 @@ import android.view.View;
 import android.widget.Button;
 
 public class StorageMainActivity extends AppCompatActivity {
-    private Button btnManageInventory, btnManageDelivery, btnManageRequests, btnManagePickUpPoints, btnManageDonations, btnSendDeliveryGuy;
+    private Button btnManageInventory, btnManageDelivery, btnManageRequests, btnManageDonations, btnSendDeliveryGuy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_storage_main);
 
+        //TODO: StorageManageDropOffPointsActivity
+
         btnManageInventory = findViewById(R.id.btnManageInventory);
         btnManageDelivery = findViewById(R.id.btnManageDelivery);
         btnManageRequests = findViewById(R.id.btnManageRequests);
-        btnManagePickUpPoints = findViewById(R.id.btnManagePickUpPoints);
         btnManageDonations = findViewById(R.id.btnManageDonations);
         btnSendDeliveryGuy = findViewById(R.id.btnSendDeliveryGuy);
 
@@ -37,13 +38,7 @@ public class StorageMainActivity extends AppCompatActivity {
             }
         });
 
-        btnManagePickUpPoints.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(StorageMainActivity.this, StorageManageDropOffPointsActivity.class);
-                startActivity(intent);
-            }
-        });
+
 
         btnManageRequests.setOnClickListener(new View.OnClickListener() {
             @Override
