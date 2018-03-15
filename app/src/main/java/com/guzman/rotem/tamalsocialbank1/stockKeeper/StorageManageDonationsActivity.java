7c91165ff54e45bc46812dddc3dd9840d839ee66
Tsur <tsur2912@gmail.com>
2018-03-15 16:49:30 +0200

    Merge branch 'master' of https://github.com/RotemAAA/Tamal
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/guzman/rotem/tamalsocialbank1/stockKeeper/StorageManageDonationsActivity.java
+++ app/src/main/java/com/guzman/rotem/tamalsocialbank1/stockKeeper/StorageManageDonationsActivity.java
@@ -16,6 +16,6 @@ public class StorageManageDonationsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_storage_manage_donations);
 
         lvManageDonations = findViewById(R.id.lvManageDonations);
-        //TODO: arrange: storage_manage_donation_my_list, and than connect it...
+        //TODO: get donations from server, populate the listview
     }
 }
