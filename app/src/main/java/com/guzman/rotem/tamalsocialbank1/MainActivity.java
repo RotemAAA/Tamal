c764a1c65a0ad67e6adf0cca741a48d85803b0be
dor tzemach <tzemachdor@gmail.com>
2018-03-17 19:11:35 +0200

    changes icons and colors

--- app/src/main/java/com/guzman/rotem/tamalsocialbank1/MainActivity.java
+++ app/src/main/java/com/guzman/rotem/tamalsocialbank1/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btnLogin;
 
 
-    ArrayList<Supply> supplies = new ArrayList<>();
+    ArrayList<Food> foods = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,20 +26,49 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        supplies.add(new Supply("מטרנה Extra Care - שלב 1", 0,"7290013083661", R.drawable.b7290013083661));
-        supplies.add(new Supply("מטרנה Extra Care - שלב 2", 0,"7290013083678", R.drawable.b7290013083678));
-        supplies.add(new Supply("מטרנה Extra Care - שלב 3", 0,"7290013083685", R.drawable.b7290013083685));
-        supplies.add(new Supply("מטרנה Extra Care Comfort", 0,"7290014258914", R.drawable.b7290014258914));
-        supplies.add(new Supply("מטרנה - חלבי - שלב 1", 0,"7290011447267", R.drawable.b7290011447267));
-        supplies.add(new Supply("מטרנה - חלבי - שלב 2", 0,"7290011447281", R.drawable.b7290011447281));
-        supplies.add(new Supply("מטרנה - חלבי - שלב 3", 0,"7290011447304", R.drawable.b7290011447304));
-        supplies.add(new Supply("מטרנה - מהדרין - שלב 1", 0,"7290011447359", R.drawable.b7290011447359));
-        supplies.add(new Supply("מטרנה - מהדרין - שלב 2", 0,"7290011447373", R.drawable.b7290011447373));
-        supplies.add(new Supply("מטרנה - מהדרין - שלב 3", 0,"7290008975551", R.drawable.b7290008975551));
-        supplies.add(new Supply("מטרנה-צמחי-מגיל לידה ואילך", 0,"7290014258716", R.drawable.b7290014258716));
-        supplies.add(new Supply("מטרנה-צמחי-מגיל 12 חודשים", 0,"7290014258730", R.drawable.b7290014258730));
-        supplies.add(new Supply("מטרנה-צמחי-מגיל 0 עד 12 חודשים", 0,"7290014258723", R.drawable.b7290014258723));
-        supplies.add(new Supply("מטרנה-צמחי-מגיל 0 עד 12 חודשים", 0,"7290014258723", R.drawable.b7290014258723));
+        foods.add(new Food("מטרנה Extra Care", "שלב 1","7290013083661", R.drawable.b7290013083661));
+        foods.add(new Food("מטרנה Extra Care", "שלב 2","7290013083678", R.drawable.b7290013083678));
+        foods.add(new Food("מטרנה Extra Care", "שלב 3","7290013083685", R.drawable.b7290013083685));
+        foods.add(new Food("מטרנה Extra Care Comfort", "מגיל לידה ואילך","7290014258914", R.drawable.b7290014258914));
+        foods.add(new Food("מטרנה - חלבי", "שלב 1","7290011447267", R.drawable.b7290011447267));
+        foods.add(new Food("מטרנה - חלבי", "שלב 2","7290011447281", R.drawable.b7290011447281));
+        foods.add(new Food("מטרנה - חלבי", "שלב 3","7290011447304", R.drawable.b7290011447304));
+        foods.add(new Food("מטרנה - מהדרין", "שלב 1","7290011447359", R.drawable.b7290011447359));
+        foods.add(new Food("מטרנה - מהדרין", "שלב 2","7290011447373", R.drawable.b7290011447373));
+        foods.add(new Food("מטרנה - מהדרין", "שלב 3","7290008975551", R.drawable.b7290008975551));
+        foods.add(new Food("מטרנה-צמחי", "מגיל לידה ואילך","7290014258716", R.drawable.b7290014258716));
+        foods.add(new Food("מטרנה-צמחי", "מגיל 12 חודשים","7290014258730", R.drawable.b7290014258730));
+        foods.add(new Food("מטרנה-צמח", "מגיל 0 עד 12 חודשים","7290014258723", R.drawable.b7290014258723));
+        foods.add(new Food("פדיאשור", "מגיל שנה עד 10 שנים","8710428021533", R.drawable.b8710428021533));
+        foods.add(new Food("סימילאק-רגיל", "שלב 1 - אריזה קטנה","5099864004205", R.drawable.b5099864004205));
+        foods.add(new Food("סימילאק-רגיל", "שלב 1 - אריזה גדולה","5099864006704", R.drawable.b5099864006704));
+        foods.add(new Food("סימילאק-רגיל", "שלב 2 - אריזה קטנה","5099864006070", R.drawable.b5099864006070));
+        foods.add(new Food("סימילאק-בטעם וניל", "שלב 2 - אריזה גדולה","8427030005461", R.drawable.b8427030005461));
+        foods.add(new Food("סימילאק-רגיל", "שלב 2 - אריזה גדולה","5099864006711", R.drawable.b5099864006711));
+        foods.add(new Food("סימילאק-רגיל", "שלב 2 - אריזה קטנה","5099864006070", R.drawable.b5099864006070));
+        foods.add(new Food("סימילאק-רגיל", "שלב 3 - אריזה קטנה","5099864005202", R.drawable.b5099864005202));
+        foods.add(new Food("סימילאק-רגיל", "שלב 3 - אריזה גדולה","5099864006728", R.drawable.b5099864006728));
+        foods.add(new Food("סימילאק Spit-up Relief", "מגיל 0 עד 12 חודשים","8427030005010", R.drawable.b8427030005010));
+        foods.add(new Food("סימילאק - Isoil", "על בסיס חלבון סויה","8710428005755", R.drawable.b8710428005755));
+        foods.add(new Food("סימילאק - NeoSure", "פגים ומשקל לידה נמוך-מגיל 0 עד 12 חודשים","8427030002699", R.drawable.b8427030002699));
+        foods.add(new Food("נוטרילון - שלב 1", "אריזה קטנה","8712400802482", R.drawable.b8712400802482));
+        foods.add(new Food("נוטרילון - שלב 1", "אריזה גדולה","8712400802499", R.drawable.b8712400802499));
+        foods.add(new Food("נוטרילון - שלב 2", "אריזה קטנה","8712400802505", R.drawable.b8712400802505));
+
+        foods.add(new Food("נוטרילון - שלב 2", "אריזה גדולה","8712400802512", R.drawable.b8712400802512));
+        foods.add(new Food("נוטרילון - שלב 3", "אריזה גדולה","8712400802536", R.drawable.b8712400802536));
+        foods.add(new Food("נוטרילון קומפורט", "מגיל לידה ואילך","7290110730109", R.drawable.b7290110730109));
+
+        foods.add(new Food("סימילאק טופ", "אריזה גדולה","5099864006773", R.drawable.b5099864006773));
+
+        foods.add(new Food("סופרסל בייבי - Baby", "שלב 1","7296073237440", R.drawable.b7296073237440));
+        foods.add(new Food("סופרסל בייבי - Baby", "שלב 2","7296073237457", R.drawable.b7296073237457));
+        foods.add(new Food("סופרסל בייבי - Baby", "שלב 3","7296073237464", R.drawable.b7296073237464));
+
+
+
+
+
 
 
 
